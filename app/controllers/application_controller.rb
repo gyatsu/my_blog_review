e20e3e6aef1811914db4fc649f49db4dad0a5969
MR.Y <programming@yatsukouji.com>
2015-06-21 14:28:44 +0900

    before_action :set_qを追加し、バグを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :set_q
+
+  def set_q
+      @q = Post.ransack(params[:q])
+      @posts = @q.result.page(params[:page])
+  end
+
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+
+
+
+
 end
